c681e6170093f09ea6e3bb6c217ea80251ca6ce5
ka1peshc <kalpesh111997@gmail.com>
2021-09-01 15:30:19 +0530

    calculating line distance using cartesion method

--- LIneComputation/Program.cs
+++ LIneComputation/Program.cs
@@ -7,7 +7,18 @@ namespace LIneComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line comparison computation");
+            Console.WriteLine("Enter Points according x1 y1 x2 y2 ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
 
+            double lineDistance;
+            int temp1 = x1 - x2;
+            int temp2 = y1 - y2;
+            lineDistance = Math.Pow(temp1,2) + Math.Pow(temp2,2);
+            lineDistance = Math.Sqrt(lineDistance);
+            Console.WriteLine("Line distance is " + lineDistance);
         }
     }
 }
