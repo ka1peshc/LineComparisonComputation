505eeccb8d0f6f5f5c27eef1f493f38213bba4b4
ka1peshc <kalpesh111997@gmail.com>
2021-09-08 10:13:49 +0530

    Implement inheritence used access modifier

---
+++ LIneComputation/CalculateDistance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIneComputation
+{
+    class CalculateDistance
+    {
+        private int x1, x2, y1, y2;
+        private double dist;
+        
+        public void takeCartesiaPoint()
+        {
+            Console.WriteLine("Enter Points according x1 y1 x2 y2 ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+        }
+        public double computeDistance()
+        {
+            int temp1 = x1 - x2;
+            int temp2 = y1 - y2;
+            dist = Math.Pow(temp1, 2) + Math.Pow(temp2, 2);
+            dist = Math.Sqrt(dist);
+            return dist;
+        }
+
+    }
+}
--- LIneComputation/Program.cs
+++ LIneComputation/Program.cs
@@ -2,41 +2,40 @@
 
 namespace LIneComputation
 {
-    class Program
+    class Program: CalculateDistance
     {
-        static double calDistance()
+        string DisplayLinesRelation(double line1,double line2)
         {
-            double dist=0;
-            Console.WriteLine("Enter Points according x1 y1 x2 y2 ");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            int temp1 = x1 - x2;
-            int temp2 = y1 - y2;
-            dist = Math.Pow(temp1, 2) + Math.Pow(temp2, 2);
-            dist = Math.Sqrt(dist);
-            return dist;
+            string result;
+            if (line1 == line2)
+            {
+                result="Lines are equal";
+            }
+            else if (line1 > line2)
+            {
+                result="line 1 is bigger line 2";
+            }
+            else
+            {
+                result="line 2 is bigger line 1";
+            }
+            return result;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line comparison computation");
-            double line1 = calDistance();
+
+            double line1, line2;
+            Program p = new Program();
+            p.takeCartesiaPoint();
+            line1 = p.computeDistance();
             Console.WriteLine("Line distance is " + line1);
-            double line2 = calDistance();
+
+            p.takeCartesiaPoint();
+            line2 = p.computeDistance();
             Console.WriteLine("Line distance is " + line2);
-            if (line1 == line2)
-            {
-                Console.WriteLine("Lines are equal");
-            }
-            else if (line1 > line2)
-            {
-                Console.WriteLine("line 1 is bigger line 2");
-            }
-            else
-            {
-                Console.WriteLine("line 2 is bigger line 1");
-            }
+
+            Console.WriteLine(p.DisplayLinesRelation(line1, line2));
 
         }
     }
